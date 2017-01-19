43fe3e11af8e5b03dc54ae23fc18c8af9603a6ea
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-19 14:20:42 -0600

    JSON format;

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  Rails.application.routes.draw do
-    namespace :api do
+  namespace :api, defaults:{ format: :json }do
 
-    end
   end
-end
+end 
