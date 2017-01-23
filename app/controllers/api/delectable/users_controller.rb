b720ddfa5165710ce04676a669cbcd015cbd8327
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-22 20:40:22 -0600

    Added create for Users;

--- app/controllers/api/delectable/users_controller.rb
+++ app/controllers/api/delectable/users_controller.rb
@@ -1,7 +1,25 @@
 class Api::Delectable::UsersController < ApplicationController
   respond_to :json
+  skip_before_filter  :verify_authenticity_token
 
   def show
     respond_with User.find(params[:id])
   end
+
+
+  def create
+    user=User.new(user_params)
+    # if the user is saved successfully than respond with json data and status code 201
+    if user.save
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors}, status: 422
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :lastname, :phone)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults:{ format: :json }do
     namespace :delectable do
       #devise_for :users
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
