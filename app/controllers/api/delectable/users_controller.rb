417a8447e9b6b01fb033106082e1a7994d80001e
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-22 19:39:55 -0600

    Modified routes.rd; Fixed users controller;

--- app/controllers/api/delectable/users_controller.rb
+++ app/controllers/api/delectable/users_controller.rb
@@ -1,2 +1,7 @@
-class UsersController < ApplicationController
+class Api::Delectable::UsersController < ApplicationController
+  respond_to :json
+
+  def show
+    respond_with User.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults:{ format: :json }do
     namespace :delectable do
-
+      #devise_for :users
+      resources :users, :only => [:show, :create]
     end
   end
 end
