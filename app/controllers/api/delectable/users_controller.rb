0f8ece2523499191dd6ddb8f8745827e6d0382ea
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-22 20:48:17 -0600

    Added update and delete;

--- app/controllers/api/delectable/users_controller.rb
+++ app/controllers/api/delectable/users_controller.rb
@@ -17,6 +17,24 @@ class Api::Delectable::UsersController < ApplicationController
     end
   end
 
+  # Updating users
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  # Deleting users
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :firstname, :lastname, :phone)
