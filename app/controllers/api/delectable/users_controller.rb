422cdb5a655494c8982660ce8039580322ba1223
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-01-22 22:29:16 -0600

    Search Users by lastname, email, or phone number;

--- app/controllers/api/delectable/users_controller.rb
+++ app/controllers/api/delectable/users_controller.rb
@@ -3,13 +3,55 @@ class Api::Delectable::UsersController < ApplicationController
   skip_before_filter  :verify_authenticity_token
 
   def index
-    u = User.all
-    render json: u, status: 200
+    users = User.all
+    render json: users, status: 200
   end
 
   def show
-    respond_with User.find(params[:id])
+    if (is_number?(params[:id]) == false && params[:id].include?('@')) # search by email
+      find_users(nil,params[:id], nil)
+
+    elsif (is_number?(params[:id]) == false) # search by lastname
+      find_users(params[:id],nil,nil)
+
+    elsif (is_number?(params[:id]) && params[:id].length == 10) # search by phone number
+        find_users(nil,nil,params[:id])
+
+    elsif (params[:id].to_i != 0) # search by id
+        user = User.find(params[:id])
+        if(user.nil? == false)
+          render json: user, status: 200
+        else
+          render json: { errors: user.errors }, status: 404
+        end
+
+    else
+      render json: 'Error: Invalid Argument - Try {lastname, phone, email}', status: 404
+    end
   end
+
+  def find_users(lname, email, phone)
+    if (email.nil? == false)
+    users = User.search_by_email(email)
+    elsif (lname.nil? == false)
+      users = User.search_by_last_name(lname)
+    elsif (phone.nil? == false)
+      users = User.search_by_phone(phone)
+
+    end
+
+    if(users.exists?)
+      render json: users, status: 200
+    else
+      render json: users.errors, status: 404
+    end
+
+  end
+
+  def is_number?(string)
+    true if Float(string) rescue false
+  end
+
 
 
   def create
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.search_by_last_name(lname)
+    where("lastname LIKE ?", "%#{lname}" )
+  end
+
+  def self.search_by_phone(phone)
+    where("phone LIKE ?", "%#{phone}" )
+  end
+
+  def self.search_by_email(email)
+    e = email + ".com"
+    where("email LIKE ?", "%#{e}" )
+  end
+
 end
