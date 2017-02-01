ee25348dbbb202cc201d605c993fe11070c8cdf8
Adeeb Ahmed <adeeb.ahmed1995@gmail.com>
2017-02-01 12:30:46 -0600

    Removed incorrect menu model;

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   devise_for :reports
   devise_for :admins
-  devise_for :menus
   devise_for :orders
   devise_for :users
   namespace :api, defaults:{ format: :json }do
     namespace :delectable do
-      #devise_for :users
       resources :users, :only => [:index, :show, :create, :update, :destroy, :search]
+      resources :menu, :only => [:index, :show, :create, :update, :destroy]
     end
   end
 end
